e4d31bda9d8e7e13d9ca7619f5f52d34a63d0737
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-06-07 16:31:59 +0530

    modifying appbar styles
    Signed-off-by: TharindaNimnajith <tharindarajapakshe@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
@@ -15,7 +16,9 @@ class AppTest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello World!'),
+      home: Text(
+        'Hello World!',
+      ),
     );
   }
 }
@@ -28,7 +31,10 @@ class App extends StatelessWidget {
       builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       title: 'Flutter in Sinhala',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        brightness: Brightness.dark,
+      ),
       home: Home(),
     );
   }
@@ -38,35 +44,130 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Learn Flutter'),
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () => {},
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
+      child: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'Learn Flutter',
+            ),
+            elevation: 20.0,
+            backgroundColor: Colors.green,
+            // flexibleSpace: Image.asset(
+            //   'assets/appbar.jpg',
+            //   fit: BoxFit.cover,
+            // ),
+            leading: IconButton(
+              icon: Icon(
+                Icons.menu,
+              ),
               onPressed: () => {},
             ),
-            IconButton(
-              icon: Icon(Icons.more_vert),
-              onPressed: () => {},
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(
+                  Icons.search,
+                ),
+                onPressed: () => {},
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.more_vert,
+                ),
+                onPressed: () => {},
+              ),
+            ],
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(
+                    Icons.directions_bike,
+                  ),
+                ),
+                Tab(
+                  icon: Icon(
+                    Icons.directions_boat,
+                  ),
+                ),
+                Tab(
+                  icon: Icon(
+                    Icons.directions_car,
+                  ),
+                ),
+                Tab(
+                  icon: Icon(
+                    Icons.directions_railway,
+                  ),
+                ),
+              ],
             ),
-          ],
-          flexibleSpace: Image.asset(
-            'assets/appbar.jpg',
-            fit: BoxFit.cover,
           ),
-        ),
-        body: Center(
-          child: Text(
-            'Flutter Sinhala Tutorial',
-            style: TextStyle(fontSize: 20.0),
+          body: TabBarView(
+            children: [
+              tab1(),
+              tab2(),
+              tab3(),
+              tab4(),
+            ],
           ),
         ),
       ),
     );
   }
+}
+
+Widget tab1() {
+  return Container(
+    child: Center(
+      child: Text(
+        'Tab 1',
+        style: TextStyle(
+          fontSize: 20.0,
+          color: Colors.lightGreen,
+        ),
+      ),
+    ),
+  );
+}
+
+Widget tab2() {
+  return Container(
+    child: Center(
+      child: Text(
+        'Tab 2',
+        style: TextStyle(
+          fontSize: 20.0,
+          color: Colors.lightGreen,
+        ),
+      ),
+    ),
+  );
+}
+
+Widget tab3() {
+  return Container(
+    child: Center(
+      child: Text(
+        'Tab 3',
+        style: TextStyle(
+          fontSize: 20.0,
+          color: Colors.lightGreen,
+        ),
+      ),
+    ),
+  );
+}
+
+Widget tab4() {
+  return Container(
+    child: Center(
+      child: Text(
+        'Tab 4',
+        style: TextStyle(
+          fontSize: 20.0,
+          color: Colors.lightGreen,
+        ),
+      ),
+    ),
+  );
 }
