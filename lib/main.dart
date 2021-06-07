0aaab758a5025db754e6790655daf71fb9fa5492
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-06-07 15:46:36 +0530

    modifying appbar styles
    Signed-off-by: TharindaNimnajith <tharindarajapakshe@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => App(), // Wrap your app
+    ),
+  );
 }
 
 class AppTest extends StatelessWidget {
@@ -17,14 +24,46 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter in Sinhala',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
         appBar: AppBar(
-          title: Text("Learn Flutter"),
+          title: Text('Learn Flutter'),
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () => {},
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () => {},
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () => {},
+            ),
+          ],
+          flexibleSpace: Image.asset(
+            'assets/appbar.jpg',
+            fit: BoxFit.cover,
+          ),
         ),
         body: Center(
           child: Text(
-            "Flutter Sinhala Tutorial",
-            style: TextStyle(fontSize: 50.0),
+            'Flutter Sinhala Tutorial',
+            style: TextStyle(fontSize: 20.0),
           ),
         ),
       ),
