8bf3a3a7bf0f1d63b4b7a4e449f39dff5488df57
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-06-07 14:46:00 +0530

    test
    Signed-off-by: TharindaNimnajith <tharindarajapakshe@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(App2());
 }
 
-class App extends StatelessWidget {
+class App1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Text('Hello World!'),
+    );
+  }
+}
+
+class App2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Learn Flutter"),
+        ),
+        body: Center(
+          child: Text(
+            "Flutter Sinhala Tutorial",
+            style: TextStyle(fontSize: 50.0),
+          ),
+        ),
+      ),
     );
   }
 }
